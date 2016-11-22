25e7b81a21fb66a3952f95108380b105ef2ed25d
Deadpool <arbitrator@Deadpools-MacBook-Pro.local>
2016-11-23 00:55:11 +0530

    Added SENDGRID online setup contacts details

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,11 +95,6 @@ Rails.application.configure do
     :password              => ENV['SENDGRID_PASSWORD'],
     :domain                => 'heroku.com',
     :enable_starttls_auto => true
-
-
-
-
-
   }
 
 end
